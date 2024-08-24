f9274717fc602872c83d3df61bf41e24fca06516
Utkarsh <utkar.sh@outlook.com>
2024-08-24 21:09:54 +0530

    use chan to process concurrently

--- 1-producer-consumer/main.go
+++ 1-producer-consumer/main.go
@@ -13,19 +13,19 @@ import (
 	"time"
 )
 
-func producer(stream Stream) (tweets []*Tweet) {
+func producer(stream Stream, tweetChan chan *Tweet) {
 	for {
 		tweet, err := stream.Next()
 		if err == ErrEOF {
-			return tweets
+			return
 		}
 
-		tweets = append(tweets, tweet)
+		tweetChan <- tweet
 	}
 }
 
-func consumer(tweets []*Tweet) {
-	for _, t := range tweets {
+func consumer(tweets chan *Tweet) {
+	for t := <- tweets; t != nil; t = <- tweets {
 		if t.IsTalkingAboutGo() {
 			fmt.Println(t.Username, "\ttweets about golang")
 		} else {
@@ -38,11 +38,12 @@ func main() {
 	start := time.Now()
 	stream := GetMockStream()
 
+	tweetChan := make(chan *Tweet)
 	// Producer
-	tweets := producer(stream)
+	go producer(stream, tweetChan)
 
 	// Consumer
-	consumer(tweets)
+	go consumer(tweetChan)
 
 	fmt.Printf("Process took %s\n", time.Since(start))
 }
